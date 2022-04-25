eabe9406b4e7f0143bcfa2a109ea90f07028aa22
David Dang 23 <dndang23@csc3-desktop.amherst.edu>
2022-04-25 13:37:09 -0400

    Added cluster programming

---
+++ src/propeller/propeller-simple_regression-probabilistic_runs.cmd
@@ -0,0 +1,19 @@
+## Global job properties 
+universe    = vanilla 
+notification = error 
+notify_user  = dndang23@amherst.edu 
+initialdir  = /mnt/scratch/dndang23/propeller/src/propeller 
+getenv = True 
+executable  = run_prob
+
+## Job properties 
+output = /home/dndang23/cluster-scratch/propeller/src/propeller/results/0_prob/out 
+error  = /home/dndang23/cluster-scratch/propeller/src/propeller/results/0_prob/err 
+log    = /home/dndang23/cluster-scratch/propeller/src/propeller/results/0_prob/log 
+queue  
+
+## Job properties 
+output = /home/dndang23/cluster-scratch/propeller/src/propeller/results/1_prob/out 
+error  = /home/dndang23/cluster-scratch/propeller/src/propeller/results/1_prob/err 
+log    = /home/dndang23/cluster-scratch/propeller/src/propeller/results/1_prob/log 
+queue
