4c79297094bb49286aa6cc5a749e72c978566851
David Dang 23 <dndang23@csc3-desktop.amherst.edu>
2022-09-30 09:22:31 -0400

    Parallel testing

---
+++ src/propeller/condor_runs/gcd/gcd_runs_11.cmd
@@ -0,0 +1,13 @@
+## Global job properties
+universe    = vanilla
+notification = error
+notify_user  = dndang23@amherst.edu
+initialdir  = /mnt/scratch/dndang23/propeller/src/propeller
+getenv = True
+executable  = gcd_run_11
+
+## Job properties
+output = /home/dndang23/cluster-scratch/propeller/src/propeller/results/gcd/prob/11/out
+error  = /home/dndang23/cluster-scratch/propeller/src/propeller/results/gcd/prob/11/err
+log    = /home/dndang23/cluster-scratch/propeller/src/propeller/results/gcd/prob/11/log
+queue
