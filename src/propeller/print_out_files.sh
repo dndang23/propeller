1c673652f7e338478ec52fbf888f1259975f068e
David Dang 23 <dndang23@csc3-desktop.amherst.edu>
2022-12-01 07:00:22 -0500

    Ran tournament and lexicase selection testing

--- src/propeller/print_out_files.sh
+++ src/propeller/print_out_files.sh
@@ -2,28 +2,28 @@
 
 date=$1
 
-echo "Probabilistic plushy output"
+echo "Probabilistic plushy (lexicase selection) output"
 for i in {1..3}
 do
 	for j in {1..5}
 	do
-		echo "(probabilistic) symbolic_regression_${i}, test ${j}"
+		echo "(probabilistic - lexicase) symbolic_regression_${i}, test ${j}"
 		sleep 1
-		cat results/${date}/symbolic_regression_${i}/prob/${j}/out
+		cat results/${date}/symbolic_regression_${i}/prob_lexicase/${j}/out
 		sleep 1
 	done
 done
 
 echo ""
 
-echo "Default plushy output"
+echo "Default plushy (tournament selection) output"
 for i in {1..3}
 do
 	for j in {1..5}
 	do
-		echo "(default) symbolic_regression_${i}, test ${j}"
+		echo "(probabilistic - tournament) symbolic_regression_${i}, test ${j}"
 		sleep 1
-		cat results/${date}/symbolic_regression_${i}/default/${j}/out
+		cat results/${date}/symbolic_regression_${i}/prob_tournament/${j}/out
 		sleep 1
 	done
 done
