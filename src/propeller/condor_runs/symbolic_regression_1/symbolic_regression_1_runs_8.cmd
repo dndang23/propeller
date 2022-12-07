7458ccd26bfb1562f056832cbb6d7f13d965ffee
dndang23 <dndang23@amherst.edu>
2022-12-07 09:00:48 -0500

    Testing with increased standard deviation in variation

--- src/propeller/condor_runs/symbolic_regression_1/symbolic_regression_1_runs_8.cmd
+++ src/propeller/condor_runs/symbolic_regression_1/symbolic_regression_1_runs_8.cmd
@@ -7,7 +7,7 @@ getenv = True
 executable  = symbolic_regression_1_run_8
 
 ## Job properties
-output = /home/dndang23/cluster-scratch/propeller/src/propeller/results/2022-11-30-23/symbolic_regression_1/prob_tournament/3/out
-error  = /home/dndang23/cluster-scratch/propeller/src/propeller/results/2022-11-30-23/symbolic_regression_1/prob_tournament/3/err
-log    = /home/dndang23/cluster-scratch/propeller/src/propeller/results/2022-11-30-23/symbolic_regression_1/prob_tournament/3/log
+output = /home/dndang23/cluster-scratch/propeller/src/propeller/results/2022-12-07-08/symbolic_regression_1/prob_tournament/3/out
+error  = /home/dndang23/cluster-scratch/propeller/src/propeller/results/2022-12-07-08/symbolic_regression_1/prob_tournament/3/err
+log    = /home/dndang23/cluster-scratch/propeller/src/propeller/results/2022-12-07-08/symbolic_regression_1/prob_tournament/3/log
 queue
