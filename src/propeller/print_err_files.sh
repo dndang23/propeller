b57badd3f4bc1df314915ab6c2d7b6d71163cbd4
David Dang 23 <dndang23@hpc-login1.hsnet.cluster>
2023-01-05 14:27:55 -0500

    Ran epsilon-lexicase selection and lexicase selection using multiple evaluation of 10

---
+++ src/propeller/print_err_files.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+date=$1
+
+echo "Probabilistic plushy (lexicase selection) output"
+for i in {1..3}
+do
+	for j in {1..5}
+	do
+		echo "(probabilistic - lexicase) symbolic_regression_${i}, test ${j}"
+		sleep 1
+		cat results/${date}/symbolic_regression_${i}/prob_lexicase/${j}/err
+		sleep 1
+	done
+done
+
+echo ""
+
+echo "Probabilistic plushy (epsilon-lexicase selection) output"
+for i in {1..3}
+do
+	for j in {1..5}
+	do
+		echo "(probabilistic - epsilon-lexicase) symbolic_regression_${i}, test ${j}"
+		sleep 1
+		cat results/${date}/symbolic_regression_${i}/prob_epsilon_lexicase/${j}/err
+		sleep 1
+	done
+done
+
