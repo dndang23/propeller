e2ff93f53e30974bfd1d4fe4241127e2d2499d86
David Dang 23 <dndang23@hpc-login1.hsnet.cluster>
2023-03-11 17:11:49 -0500

    equal_resources_3

--- src/propeller/slurm/big_slurm_runs/equal_resources_3/symbolic_regression_11/slurm_runs.sh
+++ src/propeller/slurm/big_slurm_runs/equal_resources_3/symbolic_regression_11/slurm_runs.sh
@@ -4,15 +4,15 @@ val=${1}
 num_1=${2}
 num_2=${3}
 
-cd default_increased_generations
+#cd default_increased_generations
 
-for (( i=${num_1}; i<=${num_2}; i++ ))
-do
-	sbatch symbolic_regression_${val}_run_${i}.slurm
-        sleep 1
-done
+#for (( i=${num_1}; i<=${num_2}; i++ ))
+#do
+#	sbatch symbolic_regression_${val}_run_${i}.slurm
+#        sleep 1
+#done
 
-cd ..
+#cd ..
 
 cd default_increased_population
 
@@ -24,12 +24,12 @@ done
 
 cd ..
 
-cd probabilistic
+#cd probabilistic
 
-for (( i=${num_1}; i<=${num_2}; i++ ))
-do
-	sbatch symbolic_regression_${val}_run_${i}.slurm
-        sleep 1
-done
+#for (( i=${num_1}; i<=${num_2}; i++ ))
+#do
+#	sbatch symbolic_regression_${val}_run_${i}.slurm
+#        sleep 1
+#done
 
-cd ..
+#cd ..
