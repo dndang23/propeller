a0b50ba8d7f6c10e3b7be6e2e48fff4b34b0911f
dndang23 <dndang23@amherst.edu>
2023-03-23 21:30:40 -0400

    experiment 2: 10 evaluations with 5% UMAD and 95% adjusted mutation method using new instruction set and equal computational resources

--- src/propeller/scripts/make_folders_script/make_folders.sh
+++ src/propeller/scripts/make_folders_script/make_folders.sh
@@ -8,10 +8,11 @@ for (( i=${START}; i<=${END}; i++ ))
 do
 	for j in {1..100}
 	do
-#		mkdir -p /home/dndang23/Desktop/propeller_dir/propeller/src/propeller/results/${date}/symbolic_regression_${i}/default_epsilon_lexicase_increased_generations/${j}/
-#		mkdir -p /home/dndang23/Desktop/propeller_dir/propeller/src/propeller/results/${date}/symbolic_regression_${i}/default_epsilon_lexicase_increased_population/${j}/
-		mkdir -p /home/dndang23/Desktop/propeller_dir/propeller/src/propeller/results/${date}/symbolic_regression_${i}/default/${j}/
-		mkdir -p /home/dndang23/Desktop/propeller_dir/propeller/src/propeller/results/${date}/symbolic_regression_${i}/prob/${j}/
+		mkdir -p /home/dndang23/Desktop/propeller_dir/propeller/src/propeller/results/${date}/symbolic_regression_${i}/default_epsilon_lexicase_increased_generations/${j}/
+		mkdir -p /home/dndang23/Desktop/propeller_dir/propeller/src/propeller/results/${date}/symbolic_regression_${i}/default_epsilon_lexicase_increased_population/${j}/
+		mkdir -p /home/dndang23/Desktop/propeller_dir/propeller/src/propeller/results/${date}/symbolic_regression_${i}/prob_epsilon_lexicase/${j}/
+#		mkdir -p /home/dndang23/Desktop/propeller_dir/propeller/src/propeller/results/${date}/symbolic_regression_${i}/default/${j}/
+#		mkdir -p /home/dndang23/Desktop/propeller_dir/propeller/src/propeller/results/${date}/symbolic_regression_${i}/prob/${j}/
 	done
 done
 
